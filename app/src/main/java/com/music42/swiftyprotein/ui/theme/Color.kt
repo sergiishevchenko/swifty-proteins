bb3b1a5dc4e79c2d90269e7753f423811e8ffa7d
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2026-03-09 21:04:05 +0100

    refactor Color.kt to update color palette with new brand colors and improve light/dark theme support

--- app/src/main/java/com/music42/swiftyprotein/ui/theme/Color.kt
+++ app/src/main/java/com/music42/swiftyprotein/ui/theme/Color.kt
@@ -2,14 +2,14 @@ package com.music42.swiftyprotein.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFCFBCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val BrandGreen = Color(0xFF1F8F1F)
+val BrandGreenDark = Color(0xFF166D16)
+val BrandGreenContainer = Color(0xFFDDF4DD)
 
-val Purple40 = Color(0xFF7C4DFF)
-val PurpleGrey40 = Color(0xFF625B71)
-val Pink40 = Color(0xFF7D5260)
+val LightBackground = Color(0xFFF5F8F5)
+val LightSurface = Color(0xFFFFFFFF)
+val LightSurfaceVariant = Color(0xFFE8F0E8)
 
-val DarkBackground = Color(0xFF1A1B2E)
-val DarkSurface = Color(0xFF232438)
-val DarkSurfaceVariant = Color(0xFF2D2E45)
+val DarkBackground = Color(0xFF101710)
+val DarkSurface = Color(0xFF172117)
+val DarkSurfaceVariant = Color(0xFF223022)
